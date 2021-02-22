b923a5908cd0689066b9afa5c4b0a47398908f7a
Daysi Guerra <daysi219@gmail.com>
2021-02-22 01:07:37 -0500

    .

--- opcion1/src/Components/FormUsuario.js
+++ opcion1/src/Components/FormUsuario.js
@@ -79,7 +79,7 @@ return(
         </div>
         <div className="col-md-12">         
             <label className="form-label">Email (optional)</label>
-            <input type="text" className="form-control" name="email" 
+            <input type="email" className="form-control" name="email" 
             value={value.email}
             onChange={(e) =>{actualizarInput(e)}}
             placeholder="you@example.com" />
